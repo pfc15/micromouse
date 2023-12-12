8b25d48846988b7c2da4e3bd5d496d175f30adeb
pfc15 <pedrofcrux@gmail.com>
2023-12-12 18:10:57 -0300

    novo main

---
+++ main.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <string.h>
+# include <time.h>
+
+struct no{
+    int visitado[4];
+    
+};
+
+
+
+int main(){
+    printf("oi\n");
+
+    return 0;
+}
