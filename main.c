abecdb8f62df6a26ff1ad991684552bbf35e4b6e
pfc15 <pedrofcrux@gmail.com>
2023-12-13 00:19:12 -0300

    correção de bugs de arítimética

--- main.c
+++ main.c
@@ -15,8 +15,16 @@ struct no{
     int visitado[4];
 };
 
-# define Hash(x,y, m) ((((x*x)+(3*x)+(2*x*y)+y+(y*y))/2)%m)
+int mod(int a, int b)
+{
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+# define Hash(x,y, m) (mod((((x*x)+(3*x)+(2*x*y)+y+(y*y))/2),m))
 # define COLint 10
+
+
+
 
 int htfind(no **ht, long int M, no* tf){
     int h = Hash(tf->x, tf->y, M);
@@ -27,8 +35,8 @@ int htfind(no **ht, long int M, no* tf){
 }
 
 int htfind_coordenada(no **ht, long int M, int tf_x, int tf_y){
-    int h = Hash(tf_x, tf_y, M);
-    for (int c=0, i=h;c<COLint;i =(i+1)%M, c++){
+    long int h = Hash(tf_x, tf_y, M);
+    for (long int c=0, i=h;c<COLint;i =(i+1)%M, c++){
         if (ht[i]!=NULL)
             if (ht[i]->x == tf_x && ht[i]->y==tf_y) return i;
     }
@@ -69,13 +77,13 @@ no* No(int x, int y, no* pai){
     }
     if (pai != NULL){
         if (pai->x==x){
-            pai->filhos[((pai->y+2)%y)%4] = novo;
-            pai->visitado[((pai->y+2)%y)%4] = 1;
-            novo->visitado[((pai->y+4)%y)%4] = 1;
+            pai->filhos[mod((pai->y+2)-y,4)] = novo;
+            pai->visitado[mod((pai->y+2)-y,4)] = 1;
+            novo->visitado[mod((pai->y+4)-y,4)] = 1;
         } else{
-            pai->filhos[((pai->x+1)-x)%4] = novo;
-            pai->visitado[((pai->x+1)-x)%4] = 1;
-            novo->visitado[((pai->x+3)-x)%4] = 1;
+            pai->filhos[mod(pai->x+1-x,4)] = novo;
+            pai->visitado[mod(pai->x+1-x,4)] = 1;
+            novo->visitado[mod(pai->x+3-x,4)] = 1;
         }
     }
     return novo;
@@ -90,14 +98,17 @@ int fazer(const char acao){
     return retorno;
 }
 
-void pra_frente(no* pai, int x, int y, no **visit, int direcao){
+int pra_frente(no* pai, int x, int y, no **visit, int direcao){
 
     int visitado = htfind_coordenada(visit, 100000, x, y);
     no* filho;
     if (visitado>=0){
         printf("opa, já vim aqui!\n");
-        
         filho = visit[visitado];
+        filho->visitado[mod(direcao-2, 4)] = 1;
+        int c =0;
+        for (int i=0;i<4;i++) if (filho->visitado[i]==0)c++;
+        if (c==0) return 3;
         printf("(%d, %d)\n", filho->x, filho->y);
         fflush(stdout);
     }
@@ -106,21 +117,20 @@ void pra_frente(no* pai, int x, int y, no **visit, int direcao){
         filho = No(x, y, pai);
         HTinsert(visit, 100000, filho);
     }
-        
     
-    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    printf("estou em (%d, %d) e vou andar para frente\ndirecao: %d\n", x, y, direcao);
+
 
     int retorno = -1;
     while (retorno!= 1){
+        printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+        printf("estou em (%d, %d) e vou andar para frente\ndirecao: %d\n", x, y, direcao);
         int retorno = fazer('w');
         if (retorno == 1){ // se deu certo
             if (direcao%2==0){
-                pra_frente(filho, x+((direcao-1)*-1), y, visit, direcao);
+                retorno = pra_frente(filho, x+((direcao-1)*-1), y, visit, direcao);
             } else{
-                pra_frente(filho, x, y+((direcao-2)*-1), visit, direcao);
+                retorno = pra_frente(filho, x, y+((direcao-2)*-1), visit, direcao);
             }
-            printf(", (%d, %d)", filho->x, filho->y);
         }
         
         if (retorno==0){ // se bateu na parede
@@ -130,20 +140,24 @@ void pra_frente(no* pai, int x, int y, no **visit, int direcao){
                 direcao = (direcao+1)%4;
                 cont++;
                 printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                printf("0:x+1  1:y+1  2:x-1  3:y-1\ndireção: %d\n", direcao);
+                printf("0:x+1  1:y+1  2:x-1  3:y-1\n(%d, %d) direção: %d\n", x, y, direcao);
                 for (int i=0;i<4;i++) printf("v[%d]: %d\n", i, filho->visitado[i]);
                 fazer('l');
-                if (filho->visitado[direcao]==0 || cont>4) break;
-                
+                if (filho->visitado[direcao]==0 || cont>4) break; // se cont >4 vai ter q implementar voltar para ultimo lugar
             }
-            
+            if (cont>4) break;
         } else if (retorno==2){ // se achou o final
-            printf("voltando:\n");
             no* atual = visit[htfind(visit, 100000, filho)];
-            printf("(%d, %d) %d", filho->x, filho->y, atual == filho);
+            printf("(%d, %d)", filho->x, filho->y);
+            return 2;
+        } else if (retorno ==3){
+            int c =0;
+            for (int i=0;i<4;i++) if (filho->visitado[i]==0)c++;
+            if (c==0) return 3;
             break;
-        } else break;
+        }
     }
+    return 0;
     
     
 }
